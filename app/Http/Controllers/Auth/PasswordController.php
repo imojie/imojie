a07caabaa56b1440e6985b24f2c05028575fffac
mr-jing <877131516@qq.com>
2015-10-20 17:21:36 +0800

    修复未激活账号也能发送重置密码邮件的bug

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -4,7 +4,12 @@ namespace Imojie\Http\Controllers\Auth;
 
 use Imojie\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Password;
 use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+use Cartalyst\Sentinel\Laravel\Facades\Activation;
+
 
 class PasswordController extends Controller
 {
@@ -37,6 +42,29 @@ class PasswordController extends Controller
     public function getEmail()
     {
         return view('auth.forgot');
+    }
+
+
+    public function postEmail(Request $request)
+    {
+        $this->validate($request, ['email' => 'required|email']);
+
+        $user = Sentinel::findByCredentials(['email' => $request->only('email')]);
+        if ($user && !Activation::completed($user)) {
+            return redirect()->back()->withErrors(['email' => '该邮箱注册的账号并没有激活，请重新注册']);
+        }
+
+        $response = Password::sendResetLink($request->only('email'), function (Message $message) {
+            $message->subject($this->getEmailSubject());
+        });
+
+        switch ($response) {
+            case Password::RESET_LINK_SENT:
+                return redirect()->back()->with('status', trans($response));
+
+            case Password::INVALID_USER:
+                return redirect()->back()->withErrors(['email' => trans($response)]);
+        }
     }
 
 
